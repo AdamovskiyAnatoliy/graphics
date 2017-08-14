34531a33467c6cd81f658a3fb34b605f87aca1cc
AdamovskiyAnatoliy <adamovskiy.job@gmail.com>
2017-08-14 23:27:48 +0300

    Bar and histograms

--- matplotmain.py
+++ matplotmain.py
@@ -1,18 +1,23 @@
 import matplotlib.pyplot as plt
 
-x = [1, 2, 3]
-y = [4, 5, 3]
 
-x2 = [1, 2, 3]
-y2 = [6, 3, 7]
+population_ages = [22, 55, 62, 75, 26, 74, 65, 90, 8, 30,
+				   2, 25, 56, 27, 52, 67, 46, 59, 80, 32,
+				   100, 111, 120, 130, 121, 123, 124, 125] 
 
-plt.plot(x, y, label='First line')
-plt.plot(x2, y2, label='Second line')
 
-plt.xlabel('Plot Number')
-plt.ylabel('Importent var')
+#ids = [x for x in  range(len(population_ages))]
+
+bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130]
+
+plt.hist(population_ages, bins, histtype='bar', 
+		 rwidth=0.8, label='Populations')
+
+
+plt.xlabel('x')
+plt.ylabel('y')
 
 plt.title('Intersting Graph\nCheck it out')
 plt.legend()
 
-plt.show()
+plt.show() 
