8162d15372a9b7c5685b4a7203572b0320c2cc78
AdamovskiyAnatoliy <adamovskiy.job@gmail.com>
2017-08-16 15:41:26 +0300

    Load date from file

--- matplotmain.py
+++ matplotmain.py
@@ -1,41 +1,28 @@
 import matplotlib.pyplot as plt
+import numpy as np 
+"""
+import csv
 
-days = [1, 2, 3, 4, 5]
+x = []
+y = []
 
-sleeping = [7, 8, 6, 11, 7]
-eating = [2, 3, 4, 3, 2]
-working = [7, 8, 7, 2, 2]
-playing = [8, 5, 7, 8, 13]
-
-slices = [7, 2, 2, 13]
-activities = ['sleeping', 'eating', 'working', 'playing']
-cols = ['c', 'm', 'r', 'b']
-
-plt.pie(slices, 
-		labels=activities, 
-		colors=cols, 
-		startangle=90,
-		shadow=True, 
-		explode=(0, 0.2, 0, 0),
-		autopct='%0.1f%%')
+with open('exampel.txt', 'r') as csvfile:
+	plots = csv.reader(csvfile, delimier=',')
+	for row in plots:
+		x.append(int(row[0]))
+		y.append(int(row[1]))
 
 
+plt.plot(x,y, label='Loaded from file')
+"""
+
+x, y = np.loadtxt('exampel.txt', delimiter=',', unpack=True)
+
+plt.plot(x,y, label='Loaded from file')
 
 
-
-
-#plt.stackplot(days, sleeping, eating, working, playing, 
-#			  colors=['m', 'c', 'r', 'k'])
-
-#Для точок на площині
-#x = [1, 2, 3, 4, 5, 6, 7, 8]
-#y = [5, 2, 4, 2, 1, 4, 5, 2]
-#plt.scatter(x, y, label='skitscat', color='k', 
-#			marker='*', s=100)
-
-
-#plt.xlabel('x')
-#plt.ylabel('y')
+plt.xlabel('x')
+plt.ylabel('y')
 
 plt.title('Intersting Graph\nCheck it out')
 plt.legend()
