42e4e0fa81c41c355093f308feae5cb5be525444
AdamovskiyAnatoliy <adamovskiy.job@gmail.com>
2017-08-17 22:46:55 +0300

    Customizations graph

--- matplotmain.py
+++ matplotmain.py
@@ -1,30 +1,39 @@
+import datetime
+import numpy as np
 import matplotlib.pyplot as plt
-import numpy as np 
-"""
-import csv
-
-x = []
-y = []
-
-with open('exampel.txt', 'r') as csvfile:
-	plots = csv.reader(csvfile, delimier=',')
-	for row in plots:
-		x.append(int(row[0]))
-		y.append(int(row[1]))
+import matplotlib.dates as mdates
+import matplotlib.cbook as cbook
 
 
-plt.plot(x,y, label='Loaded from file')
-"""
+datafile = cbook.get_sample_data('goog.npy')
 
-x, y = np.loadtxt('exampel.txt', delimiter=',', unpack=True)
+r = np.load(datafile, encoding='bytes').view(np.recarray)
 
-plt.plot(x,y, label='Loaded from file')
+fig = plt.figure()
+ax = plt.subplot2grid((1,1), (0,0))
 
+ax.plot_date(r.date, r.adj_close,'-',label='Price')
 
-plt.xlabel('x')
-plt.ylabel('y')
+for label in ax.xaxis.get_ticklabels():
+	label.set_rotation(45)
 
-plt.title('Intersting Graph\nCheck it out')
+datemin = datetime.date(r.date.min().year, 1, 1)
+datemax = datetime.date(r.date.max().year + 1, 1, 1)
+ax.set_xlim(datemin, datemax)
+
+ax.grid(True) # color='g', linestyle='-', linewidth=5)
+
+ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.title('Interesting Graph')
 plt.legend()
+plt.subplots_adjust(left=0.09, 
+					bottom=0.18, 
+					right=0.94, 
+					top=0.9, 
+					wspace=0.2, 
+					hspace=0)
 
-plt.show() 
+plt.show()
