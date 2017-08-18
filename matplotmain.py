ab0a0701861cf218d3b4f1489f688db57ce97cce
AdamovskiyAnatoliy <adamovskiy.job@gmail.com>
2017-08-18 23:49:09 +0300

    candlestick ohlc graph

--- matplotmain.py
+++ matplotmain.py
@@ -1,6 +1,10 @@
 import datetime
 import random
 import numpy as np
+import matplotlib.ticker as mticker
+import matplotlib.finance as fin
+
+
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.cbook as cbook
@@ -9,6 +13,7 @@ import matplotlib.cbook as cbook
 datafile = cbook.get_sample_data('goog.npy')
 
 r = np.load(datafile, encoding='bytes').view(np.recarray)
+
 
 fig = plt.figure()
 ax = plt.subplot2grid((1,1), (0,0))
@@ -27,10 +32,32 @@ def app_close(start_close, new_close):
 
 app_close(r.adj_close, close)
 
-ax.plot_date(r.date, close,'-',label='Price')
+x = 0
+y = len(r.date)
+ohlc = []
+
+while x < y:
+	append_me = r[x][0].toordinal(), r[x][1], r[x][2], r[x][3], r[x][4], r[x][5]
+	ohlc.append(append_me)
+	x += 1
+
+
+
+fin.candlestick_ohlc(ax, ohlc, width=0.4, colorup='g',alpha=0.6, colordown='r')
+
+
+for label in ax.xaxis.get_ticklabels():
+	label.set_rotation(45)
+
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+
+ax.xaxis.set_major_locator(mticker.MaxNLocator(10))
+ax.grid(True)
+"""
+ax.plot_date(r.date, close,'-',label='Price', color='c')
 ax.plot([], [], linewidth=5, label='loss', color='r', alpha=0.5 )
 ax.plot([], [], linewidth=5, label='gain', color='g', alpha=0.5 )
-ax.axhline(close[0], color='k', linewidth=5)#лінія приякомусь значені
+#ax.axhline(close[0], color='k', linewidth=5)#лінія приякомусь значені
 
 
 ax.fill_between(r.date,
@@ -38,7 +65,7 @@ ax.fill_between(r.date,
 			    close[0],
 				where=(close>close[0]), 
 				facecolor='g',
-				alpha=0.3 )
+				alpha=0.3)
 
 ax.fill_between(r.date,
 			    close, 
@@ -57,29 +84,29 @@ datemin = datetime.date(r.date.min().year, 1, 1)
 datemax = datetime.date(r.date.max().year + 1, 1, 1)
 ax.set_xlim(datemin, datemax)
 
-ax.grid(True) # color='g', linestyle='-', linewidth=5)
+ax.grid(True, color='c') # color='g', linestyle='-', linewidth=5)
 #ax.xaxis.label.set_color('c')
 #ax.yaxis.label.set_color('m')
 #440
 #ax.set_yticks([0, 300, 600, 900])
 
 ax.spines['left'].set_color('c')
+ax.spines['bottom'].set_color('c') 
 ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
-ax.spines['left'].set_linewidth(5)
+#ax.spines['left'].set_linewidth(5)
 
-ax.tick_params(axis='x', colors='#f06215')
+ax.tick_params(axis='x', colors='c')
+ax.tick_params(axis='y', colors='c')
+"""
 
+#ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
 
-
-
-ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
-
-plt.xlabel('Date')
-plt.ylabel('Price')
-plt.title('Interesting Graph')
+plt.xlabel('Date', color='c')
+plt.ylabel('Price', color='c')
+plt.title('Interesting Graph', color='c')
 plt.legend()
-plt.subplots_adjust(left=0.09, 
+plt.subplots_adjust(left=0.15, 
 					bottom=0.18, 
 					right=0.94, 
 					top=0.9, 
