fca22ac861311aa36638eabf2a35bebed22eba73
AdamovskiyAnatoliy <adamovskiy.job@gmail.com>
2017-08-18 18:13:13 +0300

    Spines and horizontal

--- matplotmain.py
+++ matplotmain.py
@@ -15,10 +15,9 @@ ax = plt.subplot2grid((1,1), (0,0))
 
 close = []
 
-
 def app_close(start_close, new_close):
 	g = np.pi/2
-	
+
 	for i in start_close:
 		i = i*(np.sin(g))
 		g += random.choice([0.05, -0.05])
@@ -31,7 +30,7 @@ app_close(r.adj_close, close)
 ax.plot_date(r.date, close,'-',label='Price')
 ax.plot([], [], linewidth=5, label='loss', color='r', alpha=0.5 )
 ax.plot([], [], linewidth=5, label='gain', color='g', alpha=0.5 )
-
+ax.axhline(close[0], color='k', linewidth=5)#лінія приякомусь значені
 
 
 ax.fill_between(r.date,
@@ -49,6 +48,8 @@ ax.fill_between(r.date,
 				alpha=0.3 )
 
 
+
+
 for label in ax.xaxis.get_ticklabels():
 	label.set_rotation(45)
 
@@ -57,10 +58,20 @@ datemax = datetime.date(r.date.max().year + 1, 1, 1)
 ax.set_xlim(datemin, datemax)
 
 ax.grid(True) # color='g', linestyle='-', linewidth=5)
-ax.xaxis.label.set_color('c')
-ax.yaxis.label.set_color('m')
+#ax.xaxis.label.set_color('c')
+#ax.yaxis.label.set_color('m')
 #440
 #ax.set_yticks([0, 300, 600, 900])
+
+ax.spines['left'].set_color('c')
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.spines['left'].set_linewidth(5)
+
+ax.tick_params(axis='x', colors='#f06215')
+
+
+
 
 ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
 
