82b725f47cc487d267000590c92c7803df889a1b
AdamovskiyAnatoliy <adamovskiy.job@gmail.com>
2017-08-24 16:23:22 +0300

    plot and scatter 3d

--- mathplotstart.py
+++ mathplotstart.py
@@ -1,40 +1,25 @@
-import random
+from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
-from matplotlib import style
-
-style.use('fivethirtyeight')
 
 fig = plt.figure()
+ax1 = fig.add_subplot(111, projection='3d')
 
-def create_plots():
-    xs = []
-    ys = []
+x = [1,2,3,4,5,6,7,8,9,10]
+y = [5,7,4,6,7,3,8,3,8,2]
+z = [1,2,4,5,3,5,2,4,6,8]
 
-    for i in range(10):
-        x = i
-        y = random.randrange(10)
+#ax1.plot_wireframe(x,y,z)
 
-        xs.append(x)
-        ys.append(y)
-    return xs, ys
- 
-ax1 = plt.subplot2grid((6,1), (0,0), rowspan=1, colspan=1)
-ax2 = plt.subplot2grid((6,1), (1,0), rowspan=4, colspan=1)
-ax3 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1)
+x2 = [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10]
+y2 = [-5,-7,-4,-6,-7,-3,-8,-3,-8,-2]
+z2 = [1,2,4,5,3,5,2,4,6,8]
+
+ax1.scatter(x,y,z, c='k', marker='0')
+ax1.scatter(x2,y2,z2, c='r', marker='o')
 
 
-#ax1 = fig.add_subplot(221)
-#ax2 = fig.add_subplot(222)
-#ax3 = fig.add_subplot(212)
-
-
-x, y = create_plots()
-ax1.plot(x,y)
-
-x, y = create_plots()
-ax2.plot(x,y)
-
-x, y = create_plots()
-ax3.plot(x,y)
+ax1.set_xlabel('x axis')
+ax1.set_ylabel('y axis')
+ax1.set_zlabel('z axis')
 
 plt.show()
