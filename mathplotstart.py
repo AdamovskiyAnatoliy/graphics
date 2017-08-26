e161ce211a8c43a0b7a043bfb33e0a4519a4dde9
AdamovskiyAnatoliy <adamovskiy.job@gmail.com>
2017-08-26 22:56:41 +0300

    App matplotstart

--- mathplotstart.py
+++ mathplotstart.py
@@ -8,41 +8,22 @@ fig = plt.figure()
 ax1 = fig.add_subplot(111, projection='3d')
 
 # x, y, z = axes3d.get_test_data()
-x, y = np.mgrid[-5:5.1:0.05, -5:5.1:0.05]
-z = np.cos(x)
 
-# print(axes3d.__file__)
+x = np.arange(-5, 5, 0.25)
+y = np.arange(-5, 5, 0.25)
+x, y = np.meshgrid(x, y)
+z = np.sin(np.sqrt(np.abs(x**3 + y**3)))
 
-# x = [1,2,3,4,5,6,7,8,9,10]
-# y = [5,7,4,6,7,3,8,3,8,2]
-# z = [1,2,4,5,3,5,2,4,6,8]
-
-# ax1.plot_wireframe(x,y,z)
-
-# x2 = [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10]
-# y2 = [-5,-7,-4,-6,-7,-3,-8,-3,-8,-2]
-# z2 = [1,2,4,5,3,5,2,4,6,8]
-
-# ax1.scatter(x,y,z, c='k', marker='o')
-# ax1.scatter(x2,y2,z2, c='r', marker='o')
-
-# x3 = [1,2,3,4,5,6,7,8,9,10]
-# y3 = [5,7,4,6,7,3,8,3,8,2]
-# z3 = np.zeros(10)
-# dx = np.ones(10)
-# dy = np.ones(10)
-# dz = [1,2,3,4,5,6,7,8,9,10]
-
-# ax1.bar3d(x3, y3, z3, dx, dy, dz)
-
-surf = ax1.plot_surface(x, y, z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False,
-                       alpha=0.3)
+surf = ax1.plot_surface(x, y, z,
+						cmap=cm.coolwarm,
+                       	linewidth=0, 
+                       	antialiased=False,
+                       	alpha=0.3)
 fig.colorbar(surf, shrink=0.5, aspect=5)
 
-cset = ax1.contourf(x, y, z, zdir='z', offset=-1, cmap=cm.coolwarm)
-cset = ax1.contourf(x, y, z, zdir='x', offset=-6, cmap=cm.coolwarm)
-cset = ax1.contourf(x, y, z, zdir='y', offset=-6, cmap=cm.coolwarm)
+# cset = ax1.contourf(x, y, z, zdir='z', offset=-100, cmap=cm.coolwarm)
+# cset = ax1.contourf(x, y, z, zdir='x', offset=-40, cmap=cm.coolwarm)
+# cset = ax1.contourf(x, y, z, zdir='y', offset=40, cmap=cm.coolwarm)
 
 # cset = ax1.contour(x, y, z, zdir='z', offset=-100, cmap=cm.coolwarm)
 # cset = ax1.contour(x, y, z, zdir='x', offset=-40, cmap=cm.coolwarm)
