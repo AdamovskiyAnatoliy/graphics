5bf19ab3139733e3c0108d67852c74caf162f7e9
anatoliy <adamovskiy.job@gmail.com>
2018-07-19 13:15:04 +0300

    Fix fractals

--- fractals.py
+++ fractals.py
@@ -2,10 +2,10 @@ import turtle
 
 
 def square_fractal(simple_len=10, deep=3):
-	'''
-	Draws Square Koch curve
-	'''
-	for degree in [90, 270, 270, 90, 0]:
+    '''
+    Draws Square Koch curve
+    '''
+    for degree in [90, 270, 270, 90, 0]:
         if deep <= 1:
             turtle.forward(simple_len)
         else:
@@ -13,17 +13,17 @@ def square_fractal(simple_len=10, deep=3):
         turtle.left(degree)
 
 def box_fractal(simple_len=10, deep=3):
-	'''
-	Draws Box fractal 
-	'''
+    '''
+    Draws Box fractal 
+    '''
     for i in range(4):
         square_fractal(simple_len, deep)
         turtle.left(90)
 
 def triangular_fractal(simple_len=3, deep=3):
-	'''
-	Draws Koch curve 
-	'''
+    '''
+    Draws Koch curve 
+    '''
     for degree in [60, 240, 60, 0]:
         if deep <= 1:
             turtle.forward(simple_len)
@@ -32,9 +32,9 @@ def triangular_fractal(simple_len=3, deep=3):
         turtle.left(degree)
 
 def koch_fractal(simple_len=10, deep=3):
-	""" 
-	Draws Koch snowflake
-	"""
+    ''' 
+    Draws Koch snowflake
+    '''
     for i in range(3):
         triangular_fractal(simple_len, deep)
         turtle.right(120)
